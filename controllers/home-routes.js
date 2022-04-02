70e224f3093c6ae8369bd6942e9bf11a63ce447d
Dan Kim <danchanyoungkim@gmail.com>
2022-04-02 18:13:43 -0400

    finish draft of home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -19,16 +19,14 @@ router.get('/', async (req, res) => {
 // get single post
 router.get('/post/:id', async (req, res) => {
   try {
-    const postData = await Post.findByPk({
-      // HINTS:
-      //  FIRST ARGUMENT IS PRIMARY KEY 'I'D PASSED IN THE END POINT
-      //  SECOND ARGUMENT IS AN OBJECT IN WHICH YOU USE PROPERTY 'INCLUDE' TO INCLUDE USER
-      //  AND COMMENT
-      // TODO: YOUR CODE HERE
-    attributes: ['id'],
+    const postData = await Post.findByPk(req.params.id, {
     include: [
       {model: User},
-      {model: Comment},
+      {model: Comment, 
+        include: {
+          User
+        }
+      },
     ],
     });
 
