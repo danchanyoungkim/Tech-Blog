f35fa760f6e2e65a3da20ccf213bd855cdea8b71
Dan Kim <danchanyoungkim@gmail.com>
2022-04-02 19:23:26 -0400

    draft for userdata

---
+++ seeds/userData.js
@@ -0,0 +1,38 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: "Carter",
+        email: "carter@email.com",
+        password: "password",
+    },
+    {
+        username: "Kat",
+        email: "kat@email.com",
+        password: "password",
+    },
+    {
+        username: "Emile",
+        email: "emile@email.com",
+        password: "password",
+    },
+    {
+        username: Jorge,
+        email: "jorge@email.com",
+        password: "password",
+    },
+    {
+        username: Jun,
+        email: "jun@email.com",
+        password: "password",
+    },
+    {
+        username: Six,
+        email: "six@email.com",
+        password: "password",
+    },
+];
+
+const userSeed = () => User.bulkCreate(userData);
+
+module.exports = userSeed;
