f683428f54aeb11fc2dda195f83c9f70fc1bd02c
Dan Kim <danchanyoungkim@gmail.com>
2022-04-02 19:43:40 -0400

    added postData

--- seeds/userData.js
+++ seeds/userData.js
@@ -4,32 +4,32 @@ const userData = [
     {
         username: "Carter",
         email: "carter@email.com",
-        password: "password",
+        password: "one",
     },
     {
         username: "Kat",
         email: "kat@email.com",
-        password: "password",
-    },
-    {
-        username: "Emile",
-        email: "emile@email.com",
-        password: "password",
-    },
-    {
-        username: "Jorge",
-        email: "jorge@email.com",
-        password: "password",
+        password: "two",
     },
     {
         username: "Jun",
         email: "jun@email.com",
-        password: "password",
+        password: "three",
+    },
+    {
+        username: "Emile",
+        email: "emile@email.com",
+        password: "four",
+    },
+    {
+        username: "Jorge",
+        email: "jorge@email.com",
+        password: "five",
     },
     {
         username: "Unknown",
         email: "unknown@email.com",
-        password: "password",
+        password: "unknown",
     },
 ];
 
