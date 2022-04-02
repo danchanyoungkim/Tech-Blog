6c9dfd037497a645124e0990ca35f44837105a0a
Dan Kim <danchanyoungkim@gmail.com>
2022-04-02 19:57:44 -0400

    Added comment data

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -10,6 +10,7 @@ router.post('/', withAuth, async (req, res) => {
       body: req.body.body,
       // TODO: SET USERID userId TO SESSION LOGGEDIN USERID
       userId: req.session.userId,
+      postId: req.body.postIdm
     });
     res.json(newComment);
   } catch (err) {
--- seeds/commentData.js
+++ seeds/commentData.js
@@ -2,6 +2,32 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        
-    }
-]
+        userId: 4,
+        body: "Hello",
+        postId: 1,
+    },
+    {
+        userId: 1,
+        body: "Hello",
+        postId: 2,
+    },
+    {
+        userId: 3,
+        body: "Hello",
+        postId: 3,
+    },
+    {
+        userId: 6,
+        body: "Hello",
+        postId: 4,
+    },
+    {
+        userId: 4,
+        body: "Hello",
+        postId: 1,
+    },
+];
+
+const commentSeed = () => Comment.bulkCreate(commentData);
+
+module.exports = commentSeed;
